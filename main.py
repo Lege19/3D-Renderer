18bc83d25451c422a562aff8c8eeef1e5b9be4d1
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-11-06 20:41:13 +0000

    first actuall commit, opens a window, creates a output texture

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("trying to use git")
+import sdl2
+import sdl2.ext
+win = sdl2.SDL_CreateWindow(b"3D-Renderer", sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, 640, 480, 0)
+if win == None:
+    raise Exception(sdl2.SDL_GetError())
+renderer = sdl2.SDL_CreateRenderer(win, -1, 0)
+if renderer == None:
+    raise Exception(sdl2.SLD_GetError())
+outputTexture = sdl2.SDL_CreateTexture(renderer, sdl2.SDL_PIXELFORMAT_RGB888, sdl2.SDL_TEXTUREACCESS_STREAMING, 640, 480)
+if outputTexture == None:
+    raise Exception(sdl2.SDL_GetError())
+sdl2.SDL_RenderCopy(renderer, outputTexture, None, None)
+sdl2.SDL_Delay(10000)
