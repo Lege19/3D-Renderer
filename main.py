01876f1ce19068a61a2c2e05bcaaf986808ca1e2
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-11-07 20:05:55 +0000

    added event handler, initialization, shutdown and screen update functions rearranged some code related to error detection

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-import sdl2
+from sdl2 import *
 import sdl2.ext
-win = sdl2.SDL_CreateWindow(b"3D-Renderer", sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, 640, 480, 0)
-if win == None:
-    raise Exception(sdl2.SDL_GetError())
-renderer = sdl2.SDL_CreateRenderer(win, -1, 0)
-if renderer == None:
-    raise Exception(sdl2.SLD_GetError())
-outputTexture = sdl2.SDL_CreateTexture(renderer, sdl2.SDL_PIXELFORMAT_RGB888, sdl2.SDL_TEXTUREACCESS_STREAMING, 640, 480)
-if outputTexture == None:
-    raise Exception(sdl2.SDL_GetError())
-sdl2.SDL_RenderCopy(renderer, outputTexture, None, None)
-sdl2.SDL_Delay(10000)
+import sys
+import ctypes
+def testFail(value):#test if a function has failed and return an error message
+    if value == None:
+        raise Exception(SDL_GetErrorMsg())
+def startUp():#initialise values
+    global win, renderer, outputTexture
+    SDL_Init(SDL_INIT_VIDEO)
+    win = SDL_CreateWindow(b"3D-Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0)
+    testFail(win)
+    renderer = SDL_CreateRenderer(win, -1, 0)
+    testFail(renderer)
+    outputTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, 640, 480)
+    testFail(outputTexture)
+def shutDown():#stops the program
+    SDL_DestroyTexture(outputTexture)
+    SDL_DestroyRenderer(renderer)
+    SDL_Quit()
+    sys.exit()
+def handleEvents():#handle any events that need handling
+    currentEvent = SDL_Event()
+    try:#try except used to prevent compiler from raising error with NoneType.type
+        while SDL_PollEvent(ctypes.byref(currentEvent)) != 0:
+            if currentEvent.type == SDL_QUIT:
+                print("closed")
+                shutDown()
+    except:
+        pass
+def updateWindow():#draw the outputTexure to the Window
+    SDL_RenderCopy(renderer, outputTexture, None, None)
+def main():
+    startUp()
+    updateWindow()
+    SDL_Delay(5000)
+    handleEvents()
+    SDL_Delay(1000)
+    shutDown()
+if __name__ == "__main__":
+    main()
