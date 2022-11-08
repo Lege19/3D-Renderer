96c44509f5e9eb6540af66eeb74281cd93bc9958
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-11-08 16:44:51 +0000

    added Vector3

--- maths.py
+++ maths.py
@@ -20,3 +20,29 @@ class Vector2:
         return a.x*b.x + a.y*b.y
     def scale(self, scalar):#multiply by a scalar
         return Vector2(self.x * scalar, self.y * scalar)
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.x**2)
+    def normalized(self):
+        magnitude = self.magnitude()
+        return Vector3(self.x / magnitude, self.y / magnitude, self.z / magnitude)
+    def normalize(self):
+        magnitude = self.magnitude()
+        self.x = self.x / magnitude
+        self.y = self.y / magnitude
+        self.z = self.z / magnitude
+    def add(a, b):
+        return Vector3(a.x+b.x, a.y+b.y, a.z+b.z)
+    def subtract(a, b):
+        return Vector3(a.x-b.x, a.y-b.y, a.z-b.z)
+    def dot(a, b):
+        return a.x*b.x + a.y*b.y + a.z+b.z
+    def cross(a, b):
+        return Vector3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x)
+    def scale(self, scalar):
+        return Vector3(self.x * scalar, self.y * scalar, self.z * scalar)
