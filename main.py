d90a3e5f189ac8afb2d2c6769285244a44a1aeb9
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-11-09 18:10:03 +0000

    I figured out how to access the screen as an array of pixels, only added a few lines of code, but an important few

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ def testFail(value):#test if a function has failed and return an error message
     if value == None:
         raise Exception(SDL_GetErrorMsg())
 def startUp():#initialise values
-    global win, outputSurface
+    global win, outputSurface, pixels
     SDL_Init(SDL_INIT_VIDEO)
     win = SDL_CreateWindow(b"3D-Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0)
     testFail(win)
     outputSurface = SDL_GetWindowSurface(win)
     testFail(outputSurface)
+    pixels = ctypes.cast(outputSurface[0].pixels, ctypes.POINTER(ctypes.c_uint32))
 def shutDown():#stops the program
     SDL_FreeSurface(outputSurface)
     SDL_Quit()
     sys.exit()
 def handleEvents():#handle any events that need handling
     currentEvent = SDL_Event()
-    try:#try except used to prevent compiler from raising error with NoneType.type
-        while SDL_PollEvent(ctypes.byref(currentEvent)) != 0:
-            if currentEvent.type == SDL_QUIT:
-                print("closed")
-                shutDown()
-    except:
-        pass
+    while SDL_PollEvent(ctypes.byref(currentEvent)) != 0:
+        if currentEvent.type == SDL_QUIT:
+            print("closed")
+            shutDown()
 def main():
     startUp()
     SDL_Delay(1000)
