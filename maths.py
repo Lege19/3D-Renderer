3c6f81a7edd35b3062424d2a871441f7787e2aa1
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-11-20 17:58:54 +0000

    fixed bug in quaternion class

--- maths.py
+++ maths.py
@@ -100,30 +100,29 @@ class TMatrix44:
         self.ty = ty
         self.tz = tz
         self.tt = tt
-class quaternion:
-    def __init__(self, w, i, j, k):
-        self.w = w
+class Quaternion:
+    def __init__(self, r, i, j, k):
+        self.r = r
         self.i = i
         self.j = j
         self.k = k
     def axisAngle(axis, angle):
         angle = angle/2
         sinAngle = math.sin(angle)
-        return quaternion(math.cos(angle), sinAngle * axis.x, sinAngle * axis.y, sinAngle * axis.z)
-    def _applyFirstHalf(self, vector):
-        i = self.w*vector.x + self.j*vector.z - self.k*vector.y
-        j = self.w*vector.y + self.k*vector.x - self.i*vector.z
-        k = self.w*vector.z + self.i*vector.y - self.j*vector.x
-        return Vector3(i, j, k)
-#    def _applySecondHalf(self, vector):#with some rearanging its the same as first half
-#        i = vector.x*self.w - vector.y*self.k + vector.z*self.j
-#        j = vector.y*self.w - vector.z*self.i + vector.x*self.k
-#        k = vector.z*self.w - vector.x*self.j + vector.y*self.i
+        return Quaternion(math.cos(angle), sinAngle * axis.x, sinAngle * axis.y, sinAngle * axis.z)
+    def complement(self):#only works for normalised quaternions
+        return Quaternion(self.r, -self.i, -self.j, -self.k)
+    def fromVector(vector):
+        return Quaternion(0, vector.x, vector.y, vector.z)
+    def toVector(self):
+        return Vector3(self.i, self.j, self.k)
     def apply(self, vector):
-        return self._applyFirstHalf(self._applyFirstHalf(vector))
-    def combine(q1, q2):
-        w = q1.w*q2.w - q1.i*q2.i - q1.j*q2.j - q1.k*q2.k
-        i = q1.w*q2.i + q1.i*q2.w + q1.j*q2.k - q1.k*q2.j
-        j = q1.w*q2.j + q1.j*q2.w + q1.k*q2.i - q1.i*q2.k
-        k = q1.w*q2.k + q1.k*q2.w + q1.i*q2.j - q1.j*q2.i
-        return quaternion(w, i, j, k)
+        qVector = Quaternion.fromVector(vector)
+        temp = Quaternion.multiply(self, qVector)
+        return Quaternion.multiply(temp, self.complement()).toVector()
+    def multiply(q1, q2):
+        r = q1.r*q2.r - q1.i*q2.i - q1.j*q2.j - q1.k*q2.k
+        i = q1.r*q2.i + q1.i*q2.r + q1.j*q2.k - q1.k*q2.j
+        j = q1.r*q2.j + q1.j*q2.r + q1.k*q2.i - q1.i*q2.k
+        k = q1.r*q2.k + q1.k*q2.r + q1.i*q2.j - q1.j*q2.i
+        return Quaternion(r, i, j, k)
