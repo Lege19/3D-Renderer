1f41384950e6945504ef941ea328f986afd6a9f2
Lege19 <111765057+Lege19@users.noreply.github.com>
2022-12-02 16:26:16 +0000

    Added a class for Camera

--- maths.py
+++ maths.py
@@ -1,4 +1,5 @@
-import numpy as np, os
+import numpy as np, os, ctypes
+from sdl2 import *
 
 class model:
     def __init__(self, verts, tris):
@@ -17,3 +18,33 @@ class model:
                     [-1, 1, 1], [-1, 1, -1], [-1, -1, 1], [-1, -1, -1]], np.float64)
         tris = np.array([], np.uint)#haven't done this yet
         return model(verts, tris)
+class scene:
+    pass
+class camera:
+    def __init__(self, fov, nearClip, farClip, w, h):
+        self.w = w
+        self.h = h
+        self.SetAspectRatio()
+        self.fov = fov
+        self.nearClip = nearClip
+        self.farClip = farClip
+        self.outputSurface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0)
+        self.output = ctypes.cast(self.outputSurface[0].pixels, ctypes.POINTRE(ctypes.c_uint32))
+        self.matrix = self.GetMatrix()
+    def SetResolution(self, w, h):
+        self.w = w
+        self.h = h
+        self.SetAspectRatio()
+    def SetAspectRatio(self):
+        self.aspectRatio = self.w/self.h
+    def GetMatrix(self):
+        a = 1/self.nearClip - 1/self.farClip
+        b = 1/self.nearClip
+        w = a/(2*np.tan(np.deg2rad(self.fov)))
+        h = w/self.aspectRatio
+        return np.array([
+            [w, 0, a/2, 0],
+            [0, h, a/2, 0],
+            [0, 0, b, -1],
+            [0, 0, a, 0]
+        ])
